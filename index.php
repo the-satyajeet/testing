c3ebb190557595e83a04acfb0cf31a60b0eb0ff2
Satyajeet Gaur <61581886+the-satyajeet@users.noreply.github.com>
2023-11-20 15:46:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if ( isset($_GET['md5']) ) {
 <li><a href="md5.php">MD5 Encoder</a></li>
 <li><a href="makecode.php">MD5 Code Maker</a></li>
 <li><a
-href="https://github.com/csev/wa4e/tree/master/code/crack"
+href="https://github.com/the-satyajeet/testing/blob/main/index.php"
 target="_blank">Source code for this application</a></li>
 </ul>
 </body>
